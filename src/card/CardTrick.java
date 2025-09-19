9a9f77f6f918044537b3dac927018face6514d7f
BaoToanPham <phambao@sheridancollege.ca>
2025-09-18 23:01:47 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -30,24 +30,18 @@ public class CardTrick {
             c.setSuit(Card.SUITS[rd.nextInt(4)]);
             magicHand[i] = c;
         }
-        
-            Scanner scan= new Scanner(System.in);
-            System.out.print("Enter the value between 1 and 13: ");
-            int cardValue = scan.nextInt();
-            System.out.print("Enter the suit (Hearts, Diamonds, Spades, Clubs): ");
-            String cardSuit = scan.nextLine();
-            
-            Card userCard = new Card();
-            userCard.setValue(cardValue);
-            userCard.setSuit(cardSuit);
+                    
+            Card luckyCard = new Card();
+            luckyCard.setValue(5);
+            luckyCard.setSuit("Hearts");
             
             for (Card c: magicHand) {
-                if(c.getValue() == userCard.getValue() && c.getSuit().equalsIgnoreCase(userCard.getSuit())){
-                    System.out.print("Your card is in the magic hand of random cards");
+                if(c.getValue() == luckyCard.getValue() && c.getSuit().equalsIgnoreCase(luckyCard.getSuit())){
+                    System.out.print("Lucky card found!");
                     return;
                 }
             }
-            System.out.print("Your card is not in the magic hand of random cards");
+            System.out.print("Lucky card didn't found");
 
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
